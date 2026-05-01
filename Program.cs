7160230132dab8da82f6e8bd2845f5834796177d
almahmud-eng <almahmudaponofficial@gmail.com>
2026-05-02 02:02:39 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 25 + 5;
+            Console.WriteLine(result);
+
+            Console.WriteLine("Result = " + (20 + 5));
+            Console.WriteLine(20 + 5*5); //operator precedence 
+            Console.WriteLine((20 + 5)*5); //operator precedence 
+
+            Console.ReadLine();
+        }
+    }
+}
